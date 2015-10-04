4cd6226c8dd9fd595b7f67c7aeed91eeecba7ab0
jeff <ampernand@gmail.com>
2015-10-04 09:13:42 -0400

    correct name

--- I2PAddr.go
+++ I2PAddr.go
@@ -104,7 +104,7 @@ func (addr I2PAddr) ToBytes() ([]byte, error) {
 // which the b32 address was generated.
 func (addr I2PAddr) Base32() string {
 	hash := sha256.New()
-  b, _ := self.ToBytes()
+  b, _ := addr.ToBytes()
 	hash.Write(b)
 	digest := hash.Sum(nil)
 	b32addr := make([]byte, 56)
